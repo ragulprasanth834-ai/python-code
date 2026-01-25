ec848768ea5da23f7b662762a812bbb13af4d8f7
ragulprasanth834-ai <ragulprasanth834@gmail.com>
2026-01-25 16:58:22 +0530

    primenumber program

---
+++ python-code/basic/prime.py
@@ -0,0 +1,18 @@
+# Get input from user
+num = int(input("Enter a number: "))
+
+# Prime numbers are greater than 1
+if num <= 1:
+    print(f"{num} is not a prime number")
+else:
+    is_prime = True
+    # Check for divisors from 2 up to sqrt(num)
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print(f"{num} is a prime number")
+    else:
+        print(f"{num} is not a prime number")
