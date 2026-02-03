b26df73cb997e79642d24d1f1120ab0f4e991463
ragulprasanth834-ai <ragulprasanth834@gmail.com>
2026-02-03 20:48:34 +0530

    done

---
+++ calculator.py
@@ -0,0 +1,17 @@
+def calculator():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    op = input("Enter operator (+ - * /): ")
+
+    if op == "+":
+        print(a + b)
+    elif op == "-":
+        print(a - b)
+    elif op == "*":
+        print(a * b)
+    elif op == "/":
+        print(a / b)
+    else:
+        print("Invalid operator")
+
+calculator()
